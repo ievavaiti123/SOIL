8297d51269f0e06684e4d86655b8cd86adc09442
ievavaiti123 <110530476+ievavaiti123@users.noreply.github.com>
2023-05-01 12:52:12 +0100

    json, species, intersect
    fixed intersect issue, (rewrote code to a simpler version)
    added rest of the data to json and the sketch
    fixed the time-slider issue, pushed materials into array

--- sketch.js
+++ sketch.js
@@ -5,7 +5,8 @@ import {CSS2DRenderer, CSS2DObject} from 'three/addons/renderers/CSS2DRenderer.j
 
 let light;
 let material1, material2, material3, material4;
-let INTERSECTED;
+let species, type, info, types, c;
+let INTERSECTED, INTERSECTED2;
 let soil;
 let sphere;
 let sphereBB;
@@ -19,6 +20,8 @@ let x = [];
 let y = [];
 let z = [];
 let x1_ = [];
+let materials = [];
+let materials2 = [];
 
 
 // setup the camera
@@ -54,8 +57,12 @@ let year98 = convertRange(output, [0, 100], [0.7, 0]);
 
 // html variables
 const h = document.getElementById('name');
+const p2 = document.getElementById('type');
 const p = document.getElementById('info');
 
+const s1 = document.getElementById('species1');
+const s2 = document.getElementById('species2');
+const s3 = document.getElementById('species3');
 
 //camera controls
 let controls = new OrbitControls(camera, renderer.domElement);
@@ -93,7 +100,7 @@ renderer.toneMappingExposure = 0.6;
 
 // load HDR texture image, sourced from https://www.hdri-hub.com/hdrishop/freesamples/freehdri/item/117-hdr-041-path-free
 const loader = new RGBELoader();
-loader.load('imgs/HDR_041_Path.hdr', function(texture){
+loader.load('imgs/HDR_041_Path_Ref.hdr', function(texture){
     texture.mapping = THREE.EquirectangularReflectionMapping;
     scene.background = texture;
     scene.environment = texture;
@@ -105,7 +112,8 @@ light = new THREE.PointLight( 0xfffafe, 1, 100 );
 light.position.set(0, 10, 10);
 scene.add( light );
 
-
+let a;
+let objects = [];
 // create raycaster and coordinates for mouse movement
 const pointer = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
@@ -117,209 +125,145 @@ const onMouseMove = (event) => {
     pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
     pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
+
     raycaster.setFromCamera(pointer, camera);
     const intersects = raycaster.intersectObjects(scene.children);
 
     // change colour if mouse is on object
     if (intersects.length > 0) {
-        if ( INTERSECTED != intersects[ 0 ].object ) {
 
-            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+        
+        for (a = 0; a < intersects.length; a++) {
+ 
+        //check if mousew intersects with object
+        objects.push(intersects[a].object)
+        intersects[a].object.currentHex = intersects[a].object.material.emissive.getHex();
+        intersects[a].object.material.emissive.setHex( 0xff0000 );
 
-            INTERSECTED = intersects[ 0 ].object;
-            
-            // get particle info
-            let objName = intersects[0].object.userData.name;
-            let objInfo = intersects[0].object.userData.info;
-            
-            //do not show names unless you can see the particles
-            if(material1.opacity > 0 && material2.opacity > 0 && material3.opacity > 0){
-            //print out the info about the particle    
+        //get particle name
+        let objName = intersects[a].object.userData.name;
+        let objInfo = intersects[a].object.userData.info;
+        let objType = intersects[a].object.userData.type;
+       
+
+        // only show names of objects that can be seen
+        if (intersects[a].object.material.opacity > 0) {
             h.className = 'tooltip show importantH';
-            h.textContent = objName;
+        h.textContent = objName;
+        p.className = 'tooltip show importantP';
+        p.textContent = objInfo;
+        p2.className = 'tooltip show importantP';
+        p2.textContent = objType;
+        }
+        //print out the info about the particle    
+        
 
-            p.className = 'tooltip show importantP';
-            p.textContent = objInfo;
-            }
 
-            // change colour when mouse collides with object
-            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            INTERSECTED.material.emissive.setHex( 0xff0000 );
-            
+
+        }
+        
+    }
+    else {
+      
+        for (let b = 0; b < objects.length; b++) {
+            //reset object colour and array
+            if ( objects[b] ) objects[b].material.emissive.setHex( 0x00000);
+            objects[b] = null
+
+            //hide text when mouse not on object
+            p.className = 'tooltip hide';
+            p2.className = 'tooltip hide';
+            h.className = 'tooltip hide';
+
         }
 
-    } else {
+    }
 
-        // colour change back if not intersects
-        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-        INTERSECTED = null;
-        p.className = 'tooltip hide';
-        
-        h.className = 'tooltip hide';
-
-        // p2.className = 'tooltip show';
-        
-        }
-    
   };
 
 
-    
- 
-
-  
-// const cube1 = new THREE.Mesh(
-//     new THREE.BoxGeometry(1,1,1),
-//     new THREE.MeshPhongMaterial({color: 0xff0000})
-// );
-// cube1.position.set(x1, 0, 0);
-
-// let cube1BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
-// cube1BB.setFromObject(cube1);
-// console.log(cube1BB);
-
-// const ball1 = new THREE.Mesh(
-//     new THREE.SphereGeometry(1),
-//     new THREE.MeshPhongMaterial({color: 0xff14933})
-// );
-// ball1.position.set(x2,0,0);
-
-//   //ball bounding sphere:
-// let ball1BB = new THREE.Sphere(ball1.position, 1);
-// console.log(ball1BB)
-
-// scene.add(ball1, cube1);
-
-// collision = false;
-// //   scene.add(ball1);
-
-// // check for collisions of particles
-// function checkCollisions() {
-//     if(cube1BB.intersectsSphere(ball1BB)) {
-//         console.log("intersected");
-//         // animation1();
-//         collision = true
-//         if (collision == true){
-//             // x1 += x1
-//             if(x1 <= 0) {
-//             x1 -= 0.1;
-//             } else if (x1 >= 0.1) {
-//                 x1 += 0.1;
-//             }
-//             cube1.position.x = x1;
-//          }
-//     } else {
-//         collision = false
-//     }
-// }
-
-
-
-
-
 function drawSoil() {
+    
 collision = false;
-let value;
-const geometry = new THREE.SphereGeometry(2, 64, 64);
-material1 = new THREE.MeshPhysicalMaterial( { 
-    roughness: 0.1,
-    metalness: 0.3,
-    transmission: 1,
-    transparent: 1,
-    opacity: yearseven,
-    color: 0xffff00 } );
-    // console.log(material1.opacity);
-    //ior: 2.33;
-material2 = new THREE.MeshPhysicalMaterial( { 
-    roughness: 0.1,
-    metalness: 0.2,
-    transmission: 1, 
-    transparent: 1,
-    opacity: yearseven,
-    color: 0xf7a9a9 } );
-material3 = new THREE.MeshPhysicalMaterial( { 
-    roughness: 0.1,
-    metalness: 0.2,
-    transmission: 1,
-    transparent: 1,
-    opacity: yearseven,
-    color: 0xc4f157 } );
-    material4 = new THREE.MeshPhysicalMaterial( { 
-        roughness: 0.1,
-        metalness: 0.2,
-        transmission: 1,
-        transparent: 1,
-        opacity: year98,
-        color: 0xc4f157 } );
+let value98;
+let value07;
+const geometry = new THREE.SphereGeometry(0.5, 64, 64);
+
+
 
 
     for (let i=0; i<soil.length; i++) {
-        value = soil[i].amount;
-        let type = soil[i].type;
-        let species = soil[i].species;
-        let year = soil[i].year;
+        value07 = soil[i].amount07;
+        value98 = soil[i].amount98;
         
-        
+        type = soil[i].type;
+        info = soil[i].info;
+        c = soil[i].color;
 
-        for (let j = 0; j < value; j ++) {
+        material1 = new THREE.MeshPhysicalMaterial( { 
+            roughness: 0.1,
+            metalness: 0.3,
+            transmission: 1,
+            transparent: 1,
+            opacity: yearseven,
+            color: c } );
+        material2 = new THREE.MeshPhysicalMaterial( { 
+            roughness: 0.1,
+            metalness: 0.3,
+            transmission: 1,
+            transparent: 1,
+            opacity: year98,
+            color: c } );
+
+            materials.push(material1)
+            materials2.push(material2)
+           
+        //types = soil[i].types;
+      
+        
+        // use same sphere positions in different years to see the difference easier
+            for (let sPos = 0; sPos <500; sPos ++) {
+                x[sPos] = getRnd(-10, 10)
+                y[sPos] = getRnd(-10, 10)
+                z[sPos] = getRnd(-10, 10)
+            }
+       ;
+        //nutrients for year 2007
+        for (let j = 0; j < value07; j ++) {
             
-           x[j] = getRnd(-10, 10)
-           y[j] = getRnd(-10, 10)
-           z[j] = getRnd(-10, 10)
-            
-            if (type == "invertebrate" && species == "Broad Taxa" && year == "2007") {
-                
+            if (type == "invertebrate" || "Carbon Concentration" || "Olsen-P" || "Nitrogen" || "Moisture") {
+              
                 sphere = new THREE.Mesh(geometry, material1);
                 sphere.position.set(x[j], y[j], z[j]);
                 scene.add(sphere);
 
+                //use data from json to give the particles names
                 sphere.userData.nutrient = true;
-                sphere.userData.name = 'Broad Taxa';
-                sphere.userData.info = 'this is some info on this particle'
-
-                
-            }
-
-            if (type == "invertebrate" && species == "Shannon Diversity" && year == "2007") {
-                
-                const sphere = new THREE.Mesh(geometry, material2);
-                sphere.position.set(x[j], y[j], z[j]);
-                scene.add(sphere);
-
-                sphere.userData.nutrient = true;
-                sphere.userData.name = 'Shannon';
-                sphere.userData.info = 'this is some info on this particle'
-
-                
-            }
-
-            if (type == "invertebrate" && species == "Mite: Springtail" && year == "2007") {
+                sphere.userData.name = type; 
+                sphere.userData.info = info; 
                
-                const sphere = new THREE.Mesh(geometry, material3);
-                sphere.position.set(x[j], y[j], z[j]);
-                scene.add(sphere);
-
-                sphere.userData.nutrient = true;
-                sphere.userData.name = 'Mite: Springtail';
-                sphere.userData.info = 'this is some info on this particle'
             }
 
-            if (type == "invertebrate" && species == "Broad Taxa" && year == "1998") {
+        }
+
+        //nutrients for year 1998
+        for (let k = 0; k < value98; k ++) {
+            
+            if (type == "invertebrate" || "Carbon Concentration" || "Olsen-P" || "Nitrogen" || "Moisture") {
                 
-                sphere = new THREE.Mesh(geometry, material4);
-                sphere.position.set(x[j], y[j], z[j]);
+                sphere = new THREE.Mesh(geometry, material2);
+                sphere.position.set(x[k], y[k], z[k]);
                 scene.add(sphere);
 
+             
+                //use data from json to give the particles names
                 sphere.userData.nutrient = true;
-                sphere.userData.name = 'Broad Taxa';
-                sphere.userData.info = 'this is some info on this particle'
-
+                sphere.userData.name = type; 
+                sphere.userData.info = info; 
                 
             }
         }
     }
-
 
 }
 
@@ -337,26 +281,22 @@ window.addEventListener('resize', onWindowResize);
 
 //animate or update
 function animate() {
-
+   
     requestAnimationFrame( animate ); 
-    
     //change opacity on input of slider
     slider.oninput = function() {
         output = this.value;
         yearseven = convertRange(output, [0, 100], [0, 0.7]);
         year98 = convertRange(output, [0, 100], [0.7, 0]);
-        material1.opacity = yearseven;
-        material2.opacity = yearseven;
-        material3.opacity = yearseven;
-        material4.opacity = year98;
-        }
         
-        //material1.opacity = yearseven
-        // console.log(output)
-    // update labels
-    // labelRenderer.render(scene, camera);
-
+       for (let i = 0; i< soil.length; i++) {
+        materials[i].opacity = yearseven;
+        materials2[i].opacity = year98;
+       }
+   
     
+        }
+   
     
 	renderer.render( scene, camera );
 };
@@ -373,7 +313,5 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    // update size of label
-    // labelRenderer.setSize(this.window.innerWidth, this.window.innerHeight);
 
 }
