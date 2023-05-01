0b0356ef67fd38f8b4738736af06cb1e3d7f8dba
ievavaiti123 <110530476+ievavaiti123@users.noreply.github.com>
2023-05-01 15:15:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,26 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls';
 import {RGBELoader} from 'three/addons/loaders/RGBELoader.js'
-import {CSS2DRenderer, CSS2DObject} from 'three/addons/renderers/CSS2DRenderer.js';
+
 
 let light;
 let material1, material2, material3, material4;
-let species, type, info, types, c;
-let INTERSECTED, INTERSECTED2;
+let isOn = false;
+
+let  type, info,  c;
+
 let soil;
 let sphere;
-let sphereBB;
-let spheresBB = [];
+
 let collision;
-let spheres = [];
-let x1 = getRnd(-3, 3);
-let x2 = getRnd(-3, 3);
+
+
 // arrays of xyz coordinates
 let x = [];
 let y = [];
 let z = [];
-let x1_ = [];
+
+//array of materials
 let materials = [];
 let materials2 = [];
 
@@ -42,18 +43,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("sketch-container").appendChild( renderer.domElement );
 
-let newValue;
 
+// create slider and map it
 let slider = document.getElementById("sliderYear");
 let output = slider.value
 let yearseven = convertRange(output, [0, 100], [0, 0.7]);
 let year98 = convertRange(output, [0, 100], [0.7, 0]);
-//let output = slider.value;
-
-          
-
-// create the html structure for the labels
-// const h2 = document.createElement('h2'); //for title
 
 // html variables
 const h = document.getElementById('name');
@@ -68,11 +63,6 @@ const s3 = document.getElementById('species3');
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-
-
-
-//const cPointLabel = new CSS2DObject(pContainer);
-//scene.add(cPointLabel);
 
   //load data file
   fetch("/json/soilinfo.json").then(function(response) {
@@ -118,14 +108,19 @@ let objects = [];
 const pointer = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
 
+
+
+
 // track mouse movement and intersection with objects
-const onMouseMove = (event) => {
+function onMouseMove(event) {
     // calculate pointer position in normalized device coordinates
     // (-1 to +1) for both components
     pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
     pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
+}
 
+function onHover() {
     raycaster.setFromCamera(pointer, camera);
     const intersects = raycaster.intersectObjects(scene.children);
 
@@ -155,11 +150,7 @@ const onMouseMove = (event) => {
         p2.className = 'tooltip show importantP';
         p2.textContent = objType;
         }
-        //print out the info about the particle    
-        
-
-
-
+       
         }
         
     }
@@ -180,6 +171,63 @@ const onMouseMove = (event) => {
     }
 
   };
+
+function onClick() {
+ 
+    raycaster.setFromCamera(pointer, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    // let isOn = false;
+    // change colour if mouse is on object
+    if (intersects.length > 0) {
+        
+        if (isOn == false) {
+            isOn = true;
+            console.log("clicked ON");
+
+            for (a = 0; a < intersects.length; a++) {
+ 
+                //check if mousew intersects with object
+                objects.push(intersects[a].object)
+                intersects[a].object.currentHex = intersects[a].object.material.emissive.getHex();
+                intersects[a].object.material.emissive.setHex( 0xff0000 );
+        
+                //get particle name
+                let objName = intersects[a].object.userData.name;
+                let objInfo = intersects[a].object.userData.info;
+                let objType = intersects[a].object.userData.type;
+               
+        
+                // only show names of objects that can be seen
+                if (intersects[a].object.material.opacity > 0) {
+                    h.className = 'tooltip show importantH';
+                h.textContent = objName;
+                p.className = 'tooltip show importantP';
+                p.textContent = objInfo;
+                p2.className = 'tooltip show importantP';
+                p2.textContent = objType;
+                }
+               
+                }
+            
+        } else if (isOn == true){
+            isOn = false;
+            console.log("clicked OFF");
+            for (let b = 0; b < objects.length; b++) {
+                //reset object colour and array
+                if ( objects[b] ) objects[b].material.emissive.setHex( 0x00000);
+                objects[b] = null
+    
+                //hide text when mouse not on object
+                p.className = 'tooltip hide';
+                p2.className = 'tooltip hide';
+                h.className = 'tooltip hide';
+    
+            }
+        }
+
+        
+    } 
+}
 
 
 function drawSoil() {
@@ -218,7 +266,7 @@ const geometry = new THREE.SphereGeometry(0.5, 64, 64);
             materials.push(material1)
             materials2.push(material2)
            
-        //types = soil[i].types;
+     
       
         
         // use same sphere positions in different years to see the difference easier
@@ -274,16 +322,34 @@ function convertRange( value, r1, r2 ) {
 }
 
 
-
 window.addEventListener('resize', onWindowResize);
+window.addEventListener( 'mousemove', onMouseMove, false);
+window.addEventListener( 'click', onClick );
 
 
+// device checker to work on all devices. Borrowed from @ regex solution, 01/05/2023: https://medium.com/simplejs/detect-the-users-device-type-with-a-simple-javascript-check-4fc656b735e1
+function isMobile() {
+    var check = false;
+    (function(a){
+      if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) 
+        check = true;
+    })(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+
+let checkDevice = isMobile();
 
 //animate or update
 function animate() {
-   
+    
     requestAnimationFrame( animate ); 
     //change opacity on input of slider
+    checkDevice = isMobile();
+    if (checkDevice == false) {
+        onHover();
+    }
+    // 
+
     slider.oninput = function() {
         output = this.value;
         yearseven = convertRange(output, [0, 100], [0, 0.7]);
@@ -294,22 +360,20 @@ function animate() {
         materials2[i].opacity = year98;
        }
    
-    
         }
    
-    
 	renderer.render( scene, camera );
 };
 
-window.addEventListener( 'pointermove', onMouseMove );
-// window.requestAnimationFrame(animate);
+
 animate();
 
-function onWindowResize() {
 
+function onWindowResize() {
+    
+    // windowW = window.innerWidth;
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
